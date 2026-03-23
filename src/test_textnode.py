45eb9b6338f7c3a9bfe0689c67f58151bd9cafd3
Vitor Pontual <vitorpguedes@me.com>
2026-03-23 09:12:40 -0300

    [C] Commit - Test

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_eq_with_url(self):
+        node = TextNode("This Link", TextType.LINK, "www.www.www")
+        nodeb = TextNode("This Link", TextType.LINK, "www.www.www")
+        self.assertEqual(node, nodeb)
+
+    def test_neq_with_url(self):
+        node=TextNode("Link2", TextType.LINK, "link1")
+        nodeb=TextNode("Link2", TextType.LINK, "link2")
+        self.assertNotEqual(node,nodeb)
+
+    def test_not_eq_type(self):
+        node= TextNode("Text A", TextType.BOLD)
+        nodeb= TextNode("Text B", TextType.ITALIC)
+
+    def  test_url_none_vs_string(self):
+        node=TextNode("Text Url", TextType.IMAGE, None)
+        nodeb=TextNode("Text B",TextType.IMAGE, "url.url ulr" )
+        self.assertNotEqual(node, nodeb)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
